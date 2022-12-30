dfcc83b8ed4362c1b1e2eff6a8094f42dd48bd48
Luca Mandelli <luca.part.mand@gmail.com>
2022-12-30 11:10:35 -0300

    testando metodo concat e pop

--- removendo-nota.js
+++ removendo-nota.js
@@ -1,7 +1,13 @@
 const notas = [10, 6, 8, 5.5, 10];
 
+const no = [2, 3, 4, 25];
+
+const novo = notas.concat(no);
+
 notas.pop();
 
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(`A média é ${media}.`);
+
+console.log(novo);
