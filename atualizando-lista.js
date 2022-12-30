10cc98891c07be367d89e81a860b83964cc06d08
Luca Mandelli <luca.part.mand@gmail.com>
2022-12-30 13:25:43 -0300

    ultima aula de arrays js

--- atualizando-lista.js
+++ atualizando-lista.js
@@ -3,3 +3,10 @@ const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
 nomes.splice(1, 2, "Rodrigo");
 
 console.log(nomes);
+
+animaisDoAquario = ["1", "2", "3", "4"];
+
+animaisDoAquario.splice(1, 0, "5");
+animaisDoAquario.splice(3, 2, "6");
+
+console.log(animaisDoAquario);
